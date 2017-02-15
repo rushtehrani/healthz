92c99136c9703628a8eb9a65160f14fcb03a9329
rushtehrani <r@inven.io>
2017-02-14 16:10:31 -0800

    always listen on / path

--- main.go
+++ main.go
@@ -8,15 +8,12 @@ import (
 )
 
 var (
-	root   = flag.String("root", "", "Customized path root.")
 	origin = flag.String("origin", "*", "Allowed origin.")
 	port   = flag.Int("port", 5000, "Port number to serve /healthz (or customized path).")
 )
 
 func main() {
 	flag.Parse()
-
-	//path := *root + "/healthz"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
