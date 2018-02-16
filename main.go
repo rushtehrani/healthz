433db4c1483c167482146216a8e86689483f745b
rushtehrani <r@onepanel.io>
2018-02-16 11:12:00 -0800

    add path

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var (
+	path   = flag.String("path", "/", "Custom path")
 	origin = flag.String("origin", "*", "Allowed origin.")
 	port   = flag.Int("port", 5000, "Port number to serve /healthz (or customized path).")
 )
@@ -15,10 +16,10 @@ var (
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 		w.Header().Set("Access-Control-Allow-Origin", *origin)
-		w.Header().Set("Access-Control-Allow-Methods", "HEAD, GET")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
 		w.WriteHeader(http.StatusNoContent)
 
 		log.Println("/")
